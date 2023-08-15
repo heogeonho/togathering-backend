c2a1207c5fe387c0375631bb10604ace2367ca1d
heogeonho <geonh1016@gmail.com>
2023-08-16 01:31:32 +0900

    폴더 구조 최적화 및 js 생성

--- app/app.js
+++ app/app.js
@@ -5,11 +5,14 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> middleware를 등록해주는 메서드
 
