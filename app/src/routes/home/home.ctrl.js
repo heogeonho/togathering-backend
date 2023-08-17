3405bef5bf7fb4c7f02c5a78ddfc799789ca6974
heogeonho <geonh1016@gmail.com>
2023-08-17 22:49:07 +0900

    회원가입 라우팅 구현 추후 수정 예정

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -8,6 +8,10 @@ const output = {
    login: (req, res) => {
       res.render("home/login");
    },
+
+   register: (req, res) => {
+      res.render("home/register");
+   },
 };
 
 const process = {
@@ -15,6 +19,12 @@ const process = {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response);
+   },
+
+   register: (req, res) => {
+      const user = new User(req.body);
+      const response = user.register();
+      return res.json(response);
    },
 };
 
